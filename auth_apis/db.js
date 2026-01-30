9ed3f1b7c08701d976f2f38c5fb8c596d0e069e1
atharvajadhav-19 <atharva.jadhav@winvesta.in>
2026-01-30 13:52:41 +0530

    New commit

--- auth_apis/db.js
+++ auth_apis/db.js
@@ -4,6 +4,8 @@ mongoose.connect('mongodb://localhost:27017/itm_flutter_auth_app');
 
 const db = mongoose.connection;
 
+
+
 db.on('connected', () => {
     console.log('MongoDB connected successfully');
 });
